f1ee91a538439b5fa4f3e624db4858d16eff70bb
Sebastian Torres <sebastian_torres@hotmail.se>
2020-10-11 17:00:18 +0000

    bugfix remove & update onclick, also css updates

--- static/js/bag.js
+++ static/js/bag.js
@@ -50,7 +50,6 @@ $(".update-link").click(function (e) {
 
 // Remove item and reload on click
 $(".remove-item").click(function (e) {
-  var csrfToken = "{{ csrf_token }}";
   var itemId = $(this).attr("id").split("remove_")[1];
   var url = `/bag/remove/${itemId}/`;
   var data = { csrfmiddlewaretoken: csrfToken, 'itemId' : itemId};
